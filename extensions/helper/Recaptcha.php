eb846c307953278ed31d8b63644355f8fa20bcd3
Djordje Kovacevic <djordje.bigjoe@gmail.com>
2012-07-28 21:07:16 +0200

    Updated Recaptcha helper to support configuring RecaptchaOptions
    Added ability to use `RecaptchaOptions` to configure look and feel of
    reCAPTCHA challenge field. Helper can use global config set on library
    adding or local passed as param to `challenge` method

--- extensions/helper/Recaptcha.php
+++ extensions/helper/Recaptcha.php
@@ -10,6 +10,7 @@ namespace li3_recaptcha\extensions\helper;
 use li3_recaptcha\security\Recaptcha as RecaptchaLib;
 use lithium\core\Libraries;
 use lithium\core\ConfigException;
+use lithium\util\Set;
 
 class Recaptcha extends \lithium\template\Helper {
 	
@@ -27,9 +28,7 @@ class Recaptcha extends \lithium\template\Helper {
 	 */
 	protected $_strings = array(
 		'challenge' => '
-			<script type="text/javascript">
-				var RecaptchaOptions = {theme: "white"}
-			</script>
+			{:config}
 			<script type="text/javascript" src="{:src}/challenge?k={:publickey}{:errorpart}"></script>
 			<noscript>
 				<iframe src="{:src}/noscript?k=k{:publickey}{:errorpart}" height="300" width="500" frameborder="0"></iframe><br />
@@ -47,10 +46,47 @@ class Recaptcha extends \lithium\template\Helper {
 	);
 	
 	/**
+	 * Create `RecaptchaOptions` javascript object if you have additional options configured
+	 * @param array $options `'key' => 'value'` pairs to be converted to javascript
+	 * object as `RecaptchaOptions`
+	 * @see https://developers.google.com/recaptcha/docs/customization
+	 * @return null|string return null if you don't have additional options
+	 * or script with `RecaptchaOptions` object
+	 */
+	protected function _recaptchaOptions(array $options = array()) {
+		$defaults = Libraries::get('li3_recaptcha', 'options');
+		
+		if ($defaults) {
+			$options = Set::merge($defaults, $options);
+		}
+		
+		if (empty($options)) {
+			return null;
+		}
+		
+		$script  = '<script type="text/javascript">';
+		$script .= 'var RecaptchaOptions = ' . json_encode($options) . ';';
+		$script .= '</script>';
+		return $script;
+	}
+
+	/**
 	 * Create reCAPTCHA challenge field
+	 * If you pass $options or you have set `'options'` key on library adding
+	 * create `RecaptchaOptions` object to configure look and feel of your
+	 * challenge field.
+	 * 
+	 * Example:
+	 *	{{{
+	 *		$this->challenge(array('theme' => 'white'));
+	 *	}}}
+	 * 
+	 * @param array $options `'key' => 'value'` pairs to be converted to javascript
+	 * object as `RecaptchaOptions`
+	 * @see https://developers.google.com/recaptcha/docs/customization
 	 * @return string Rendered reCAPTCHA challenge field template
 	 */
-	public function challenge() {
+	public function challenge(array $options = array()) {
 		$config = Libraries::get('li3_recaptcha', 'keys');
 		$src = $this->_context->request()->scheme .  '://www.google.com/recaptcha/api';
 		$errorpart = '';
@@ -58,6 +94,7 @@ class Recaptcha extends \lithium\template\Helper {
 			$errorpart = '&amp;error=' . static::$error;
 		}
 		return $this->_render(__METHOD__, 'challenge', array(
+			'config' => $this->_recaptchaOptions($options),
 			'src' => $src,
 			'publickey' => $config['public'],
 			'errorpart' => $errorpart
